c42f89ecf1d9c3ab0c042548958fe7d5027d1116
NNT9x <42613109+nnt9x@users.noreply.github.com>
2021-12-09 20:31:28 +0700

    update

--- app/src/main/java/com/bkacad/nnt/broadcastreceiverdemoc01k10t/MainActivity.java
+++ app/src/main/java/com/bkacad/nnt/broadcastreceiverdemoc01k10t/MainActivity.java
@@ -2,10 +2,14 @@ package com.bkacad.nnt.broadcastreceiverdemoc01k10t;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.Notification;
 import android.content.IntentFilter;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +19,10 @@ public class MainActivity extends AppCompatActivity {
     private MyReceiver myReceiver;
     private IntentFilter intentFilter;
 
+    private Notification notification;
+    private NotificationManagerCompat notificationManager;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +30,14 @@ public class MainActivity extends AppCompatActivity {
 
         tvStatus = findViewById(R.id.tvStatus);
         rootView = findViewById(R.id.rootView);
+
+        notificationManager = NotificationManagerCompat.from(this);
+        // Tạo trước notification
+        notification = new NotificationCompat.Builder(this,"channel 1")
+                .setContentTitle("Thông báo")
+                .setSmallIcon(R.drawable.ic_baseline_warning_24)
+                .setContentText("Ứng dụng chỉ hoạt động khi có wifi")
+                .build();
 
         // Tạo receiver
         myReceiver = new MyReceiver() {
@@ -36,11 +52,25 @@ public class MainActivity extends AppCompatActivity {
                 tvStatus.setText("Rút sạc");
                 rootView.setBackgroundResource(android.R.color.holo_red_light);
             }
+
+            @Override
+            public void wifiOn() {
+                    notificationManager.cancel(1);
+//                Toast.makeText(MainActivity.this,"Wifi bật",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void wifiOff() {
+                // wifi tắt -> hiển thị thông báo
+                notificationManager.notify(1, notification);
+//                Toast.makeText(MainActivity.this,"Wifi tắt",Toast.LENGTH_SHORT).show();
+            }
         };
         // Tạo ra intent filter ->đk sự kiện sẽ lắng nghe
         intentFilter = new IntentFilter();
         intentFilter.addAction("android.intent.action.ACTION_POWER_CONNECTED");
         intentFilter.addAction("android.intent.action.ACTION_POWER_DISCONNECTED");
+        intentFilter.addAction("android.net.wifi.WIFI_STATE_CHANGED");
     }
 
     // Đăng kí sự kiện lắng nghe khi app run
--- app/src/main/java/com/bkacad/nnt/broadcastreceiverdemoc01k10t/MyReceiver.java
+++ app/src/main/java/com/bkacad/nnt/broadcastreceiverdemoc01k10t/MyReceiver.java
@@ -3,23 +3,34 @@ package com.bkacad.nnt.broadcastreceiverdemoc01k10t;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.widget.Toast;
 
 public abstract class MyReceiver extends BroadcastReceiver {
 
     public abstract void powerOn();
     public abstract void powerOff();
+    public abstract void wifiOn();
+    public abstract void wifiOff();
 
     @Override
     public void onReceive(Context context, Intent intent) {
         switch (intent.getAction()){
             case "android.intent.action.ACTION_POWER_CONNECTED":
                 powerOn();
-//                Toast.makeText(context, "Cắm sạc", Toast.LENGTH_SHORT).show();
                 break;
             case "android.intent.action.ACTION_POWER_DISCONNECTED":
                 powerOff();
-//                Toast.makeText(context, "Rút sạc", Toast.LENGTH_SHORT).show();
+                break;
+            case "android.net.wifi.WIFI_STATE_CHANGED":
+                WifiManager wifiManager = (WifiManager) context.getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+                if(wifiManager.isWifiEnabled()){
+                    wifiOn();
+                }
+                else{
+                    wifiOff();
+                }
+//                Toast.makeText(context, "Wifi đã bật hoặc tắt", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
